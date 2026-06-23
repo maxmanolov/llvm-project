822c2682b0266721e550abc2205fc669e2e3722d
Johannes Doerfert <jdoerfert.llvm@gmail.com>
2026-06-23 16:36:41 -0700

    [Instrumentor] Add subtype IDs to complement type IDs for vectors/arrays
    If the type of an argument passed to the instrumentation is a vector or
    array, we still want to filter on the underlying type, and the
    instrumentation might also need to know. Thus, we can now pass a subtype
    ID, which is -1 except if it's a vector or array, then it's the element
    type ID. Structs need to be handled differently.

--- llvm/lib/Transforms/IPO/Instrumentor.cpp
+++ llvm/lib/Transforms/IPO/Instrumentor.cpp
@@ -154,6 +154,19 @@ Constant *getCI(Type *IT, Ty Val, bool IsSigned = false) {
   return ConstantInt::get(IT, Val, IsSigned);
 }
 
+Constant *getSubTypeID(Type &OpTy, Type &ReqTy) {
+  switch (OpTy.getTypeID()) {
+  case Type::TypeID::ArrayTyID:
+  case Type::TypeID::FixedVectorTyID:
+  case Type::TypeID::ScalableVectorTyID:
+    return getCI(&ReqTy, OpTy.getContainedType(0)->getTypeID());
+  default:
+    break;
+  }
+
+  return getCI(&ReqTy, -1, /*IsSigned=*/true);
+}
+
 /// The core of the instrumentor pass, which instruments the module as the
 /// instrumentation configuration mandates.
 class InstrumentorImpl final {
@@ -1011,6 +1024,11 @@ static Value *getTypeId(Value &V, Type &Ty, InstrumentationConfig &IConf,
   return getCI(&Ty, V.getType()->getTypeID());
 }
 
+static Value *getSubTypeId(Value &V, Type &Ty, InstrumentationConfig &IConf,
+                           InstrumentorIRBuilderTy &IIRB) {
+  return getSubTypeID(*V.getType(), Ty);
+}
+
 static Value *getSize(Value &V, Type &Ty, InstrumentationConfig &IConf,
                       InstrumentorIRBuilderTy &IIRB) {
   auto &I = cast<Instruction>(V);
@@ -1139,8 +1157,14 @@ void StoreIO::init(InstrumentationConfig &IConf, InstrumentorIRBuilderTy &IIRB,
   }
   if (Config.has(PassValueTypeId)) {
     IRTArgs.push_back(IRTArg(IIRB.Int32Ty, "value_type_id",
-                             "The type id of the stored value.", IRTArg::NONE,
+                             "The type id of the stored value.", IRTArg::TYPEID,
                              getValueTypeId));
+  }
+  if (Config.has(PassValueSubTypeId)) {
+    IRTArgs.push_back(IRTArg(
+        IIRB.Int32Ty, "value_sub_type_id",
+        "The type id of the stored value (for arrays and vectors, or -1).",
+        IRTArg::TYPEID, getValueSubTypeId));
   }
   if (Config.has(PassAtomicityOrdering)) {
     IRTArgs.push_back(IRTArg(IIRB.Int32Ty, "atomicity_ordering",
@@ -1213,6 +1237,13 @@ Value *StoreIO::getValueTypeId(Value &V, Type &Ty, InstrumentationConfig &IConf,
   return getCI(&Ty, SI.getValueOperand()->getType()->getTypeID());
 }
 
+Value *StoreIO::getValueSubTypeId(Value &V, Type &Ty,
+                                  InstrumentationConfig &IConf,
+                                  InstrumentorIRBuilderTy &IIRB) {
+  auto &SI = cast<StoreInst>(V);
+  return getSubTypeID(*SI.getValueOperand()->getType(), Ty);
+}
+
 Value *StoreIO::getAtomicityOrdering(Value &V, Type &Ty,
                                      InstrumentationConfig &IConf,
                                      InstrumentorIRBuilderTy &IIRB) {
@@ -1276,8 +1307,14 @@ void LoadIO::init(InstrumentationConfig &IConf, InstrumentorIRBuilderTy &IIRB,
   }
   if (Config.has(PassValueTypeId)) {
     IRTArgs.push_back(IRTArg(IIRB.Int32Ty, "value_type_id",
-                             "The type id of the loaded value.", IRTArg::NONE,
+                             "The type id of the loaded value.", IRTArg::TYPEID,
                              getValueTypeId));
+  }
+  if (Config.has(PassValueSubTypeId)) {
+    IRTArgs.push_back(IRTArg(
+        IIRB.Int32Ty, "value_sub_type_id",
+        "The sub type id of the loaded value (for arrays and vectors, or -1).",
+        IRTArg::TYPEID, getValueSubTypeId));
   }
   if (Config.has(PassAtomicityOrdering)) {
     IRTArgs.push_back(IRTArg(IIRB.Int32Ty, "atomicity_ordering",
@@ -1347,6 +1384,13 @@ Value *LoadIO::getValueTypeId(Value &V, Type &Ty, InstrumentationConfig &IConf,
                               InstrumentorIRBuilderTy &IIRB) {
   auto &LI = cast<LoadInst>(V);
   return getCI(&Ty, LI.getType()->getTypeID());
+}
+
+Value *LoadIO::getValueSubTypeId(Value &V, Type &Ty,
+                                 InstrumentationConfig &IConf,
+                                 InstrumentorIRBuilderTy &IIRB) {
+  auto &LI = cast<LoadInst>(V);
+  return getSubTypeID(*LI.getType(), Ty);
 }
 
 Value *LoadIO::getAtomicityOrdering(Value &V, Type &Ty,
@@ -1625,12 +1669,21 @@ void CastIO::init(InstrumentationConfig &IConf, InstrumentorIRBuilderTy &IIRB,
     Config = *UserConfig;
   bool IsPRE = getLocationKind() == InstrumentationLocation::INSTRUCTION_PRE;
   if (Config.has(PassInput))
-    IRTArgs.push_back(IRTArg(IIRB.Int64Ty, "input", "Input value of the cast.",
-                             IRTArg::POTENTIALLY_INDIRECT, getInput));
+    IRTArgs.push_back(
+        IRTArg(IIRB.Int64Ty, "input", "Input value of the cast.",
+               IRTArg::POTENTIALLY_INDIRECT |
+                   (Config.has(PassResultSize) ? IRTArg::INDIRECT_HAS_SIZE
+                                               : IRTArg::NONE),
+               getInput));
   if (Config.has(PassInputTypeId))
     IRTArgs.push_back(IRTArg(IIRB.Int32Ty, "input_type_id",
-                             "The type id of the input value.", IRTArg::NONE,
+                             "The type id of the input value.", IRTArg::TYPEID,
                              getInputTypeId));
+  if (Config.has(PassInputSubTypeId))
+    IRTArgs.push_back(IRTArg(
+        IIRB.Int32Ty, "input_sub_type_id",
+        "The sub type id of the input value (for arrays and vectors, or -1).",
+        IRTArg::TYPEID, getInputSubTypeId));
   if (Config.has(PassInputSize))
     IRTArgs.push_back(IRTArg(IIRB.Int32Ty, "input_size",
                              "The size of the input value.", IRTArg::NONE,
@@ -1638,12 +1691,19 @@ void CastIO::init(InstrumentationConfig &IConf, InstrumentorIRBuilderTy &IIRB,
   if (!IsPRE && Config.has(PassResult))
     IRTArgs.push_back(
         IRTArg(IIRB.Int64Ty, "result", "Result of the cast.",
-               IRTArg::REPLACABLE | IRTArg::POTENTIALLY_INDIRECT, getValue,
-               Config.has(ReplaceResult) ? replaceValue : nullptr));
+               (IRTArg::REPLACABLE | IRTArg::POTENTIALLY_INDIRECT) |
+                   (Config.has(PassResultSize) ? IRTArg::INDIRECT_HAS_SIZE
+                                               : IRTArg::NONE),
+               getValue, Config.has(ReplaceResult) ? replaceValue : nullptr));
   if (Config.has(PassResultTypeId))
     IRTArgs.push_back(IRTArg(IIRB.Int32Ty, "result_type_id",
-                             "The type id of the result value.", IRTArg::NONE,
+                             "The type id of the result value.", IRTArg::TYPEID,
                              getResultTypeId));
+  if (Config.has(PassResultSubTypeId))
+    IRTArgs.push_back(IRTArg(
+        IIRB.Int32Ty, "result_sub_type_id",
+        "The sub type id of the result value (for arrays and vectors, or -1).",
+        IRTArg::TYPEID, getResultSubTypeId));
   if (Config.has(PassResultSize))
     IRTArgs.push_back(IRTArg(IIRB.Int32Ty, "result_size",
                              "The size of the result value.", IRTArg::NONE,
@@ -1669,6 +1729,13 @@ Value *CastIO::getInputTypeId(Value &V, Type &Ty, InstrumentationConfig &IConf,
   return getCI(&Ty, CI.getSrcTy()->getTypeID());
 }
 
+Value *CastIO::getInputSubTypeId(Value &V, Type &Ty,
+                                 InstrumentationConfig &IConf,
+                                 InstrumentorIRBuilderTy &IIRB) {
+  auto &CI = cast<CastInst>(V);
+  return getSubTypeID(*CI.getSrcTy(), Ty);
+}
+
 Value *CastIO::getInputSize(Value &V, Type &Ty, InstrumentationConfig &IConf,
                             InstrumentorIRBuilderTy &IIRB) {
   auto &CI = cast<CastInst>(V);
@@ -1680,6 +1747,13 @@ Value *CastIO::getResultTypeId(Value &V, Type &Ty, InstrumentationConfig &IConf,
                                InstrumentorIRBuilderTy &IIRB) {
   auto &CI = cast<CastInst>(V);
   return getCI(&Ty, CI.getDestTy()->getTypeID());
+}
+
+Value *CastIO::getResultSubTypeId(Value &V, Type &Ty,
+                                  InstrumentationConfig &IConf,
+                                  InstrumentorIRBuilderTy &IIRB) {
+  auto &CI = cast<CastInst>(V);
+  return getSubTypeID(*CI.getDestTy(), Ty);
 }
 
 Value *CastIO::getResultSize(Value &V, Type &Ty, InstrumentationConfig &IConf,
@@ -1770,8 +1844,13 @@ void NumericIO::init(InstrumentationConfig &IConf,
       (Config.has(PassSize) ? IRTArg::INDIRECT_HAS_SIZE : IRTArg::NONE);
   if (Config.has(PassTypeId))
     IRTArgs.push_back(IRTArg(IIRB.Int32Ty, "type_id",
-                             "The operation's type id.", IRTArg::NONE,
+                             "The operation's type id.", IRTArg::TYPEID,
                              getTypeId));
+  if (Config.has(PassSubTypeId))
+    IRTArgs.push_back(
+        IRTArg(IIRB.Int32Ty, "sub_type_id",
+               "The operation's sub type id (for arrays and vectors, or -1).",
+               IRTArg::TYPEID, getSubTypeId));
   if (Config.has(PassSize))
     IRTArgs.push_back(IRTArg(IIRB.Int32Ty, "size", "The operation's type size.",
                              IRTArg::NONE, getSize));
